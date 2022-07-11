66659f33e69f367987db6febc4f51c1a7cb29103
Carlos León <carlos.leon@meru.com>
2022-07-10 23:17:03 -0500

    refacto(engine): Rename `ReactterScopeWidget` to `ReactterWrapperWidget`

---
+++ packages/flutter_reactter/lib/engine/reactter_wrapper_widget.dart
@@ -0,0 +1,36 @@
+part of '../../widgets.dart';
+
+/// Abstract class to implementing a wrapper widget
+abstract class ReactterWrapperWidget implements Widget {
+  @override
+  ReactterWrapperElementMixin createElement();
+}
+
+/// Mixin to [ReactterWrapperWidget]'s Element
+mixin ReactterWrapperElementMixin<T extends StatelessWidget> on Element {
+  ReactterNestedElement? parent;
+
+  final nodes = <ReactterNestedElement>{};
+
+  @override
+  T get widget => super.widget as T;
+
+  @override
+  void mount(Element? parent, dynamic newSlot) {
+    if (parent is ReactterNestedElement?) {
+      this.parent = parent;
+    }
+    super.mount(parent, newSlot);
+  }
+
+  @override
+  void activate() {
+    super.activate();
+    visitAncestorElements((parent) {
+      if (parent is ReactterNestedElement) {
+        this.parent = parent;
+      }
+      return false;
+    });
+  }
+}
