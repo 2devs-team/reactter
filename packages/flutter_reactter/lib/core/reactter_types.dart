c37991664355dbeb0a2fc1ee7fdbf78d69864d71
Carlos León <carlos.leon@meru.com>
2022-11-03 13:13:57 -0600

    refactor(core): remove typedef innecesary

--- packages/flutter_reactter/lib/core/reactter_types.dart
+++ packages/flutter_reactter/lib/core/reactter_types.dart
@@ -2,9 +2,11 @@ part of '../core.dart';
 
 typedef SelectorAspect<T> = bool Function(InheritedElement inheritedElement);
 
-typedef ListenHooks<T> = List<ReactterHook> Function(T instance);
+typedef ListenHooks<T> = List<ReactterState> Function(T instance);
 
 typedef InstanceBuilder<T> = Widget Function(
-    T inst, BuildContext context, Widget? child);
+  T inst,
+  BuildContext context,
+  Widget? child,
+);
 
-typedef OnInitContext<T> = void Function(T instance);
