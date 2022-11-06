2ab03608282a3c83d88f7f7e9062b714a87cfce8
Carlos León <carlos.leon@meru.com>
2022-11-06 01:59:31 -0600

    doc(widgets): Add documentation to `ReactterWatcher`.

--- packages/flutter_reactter/lib/widgets/reactter_watcher.dart
+++ packages/flutter_reactter/lib/widgets/reactter_watcher.dart
@@ -1,5 +1,67 @@
 part of '../widgets.dart';
 
+/// A [StatefulWidget] that listens for [Signal]s and re-build when any [Signal] is changed.
+///
+/// For example:
+///
+/// ```dart
+/// final count = 0.signal;
+/// final toggle = false.signal;
+///
+/// class Example extends StatelessWidget {
+///   ...
+///   Widget build(context) {
+///     return ReactterWatcher(
+///       builder: (context, child) {
+///         return Column(
+///           children: [
+///             Text("Count: $count"),
+///             Text("Toggle is: $toggle"),
+///           ],
+///         );
+///       },
+///     );
+///   }
+/// }
+/// ```
+///
+/// Build the widget tree with the values of the [Signal]s contained in
+/// the [ReactterWatcher] [builder], and with each change of its values,
+/// it will re-build the widget tree.
+///
+/// **CONSIDER** Use [child] property to pass a [Widget] that
+/// you want to build it once. The [ReactterWatcher] pass it through
+/// the [builder] callback, so you can incorporate it into your build:
+///
+/// ```dart
+/// ReactterWatcher(
+///   child: Column(
+///     children: [
+///       ElevatedButton(
+///         onPressed: () => signal.value += 1,
+///         child: const Text("Increase +1"),
+///       ),
+///       ElevatedButton(
+///         onPressed: () => toggle(!toggle.value),
+///         child: const Text("Toggle"),
+///       ),
+///     ],
+///   ),
+///   builder: (context, child) {
+///     return Column(
+///       children: [
+///         Text("Count: $count"),
+///         Text("Toggle is: $toggle"),
+///         child, // Column with 2 buttons
+///       ],
+///     );
+///   },
+/// );
+/// ```
+///
+/// See also:
+///
+/// * [Signal], a reactive state of any type.
 class ReactterWatcher extends StatefulWidget {
   /// Provides a widget , which render one time.
   ///
@@ -57,11 +119,9 @@ class _ReactterWatcherState extends State<ReactterWatcher>
     Reactter.signalProxy = this;
   }
 
-  _onSignalDidUpdate(_, __) {
-    setState(() {});
-  }
+  void _onSignalDidUpdate(_, __) => setState(() {});
 
-  _clearSignals() {
+  void _clearSignals() {
     for (var signal in _signals) {
       Reactter.off(signal, Lifecycle.didUpdate, _onSignalDidUpdate);
     }
