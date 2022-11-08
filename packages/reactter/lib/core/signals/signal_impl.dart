c7c863526f79dedf84360dc156781a8c6c38e2c3
Carlos León <carlos.leon@meru.com>
2022-11-07 23:44:19 -0600

    doc(core): Add documentation to `Signal`.

--- packages/reactter/lib/core/signals/signal_impl.dart
+++ packages/reactter/lib/core/signals/signal_impl.dart
@@ -1,5 +1,69 @@
 part of '../../core.dart';
 
+/// A base-class that can be used to store a value of [T]
+/// and notify the listeners when the value is updated.
+///
+/// You can create a new [Signal]:
+///
+/// ```dart
+/// // usign `.signal` extension
+/// final strSignal = "initial value".signal;
+/// final intSignal = 0.signal;
+/// final userSignal = User().signal;
+///
+/// // or usign the constructor class
+/// final strSignal = Signal<String>("initial value");
+/// final intSignal = Signal<int>(0);
+/// final userSignal = Signal<User>(User());
+/// ```
+///
+/// You can get the [value]:
+///
+/// ```dart
+/// // usign a `value` getter
+/// print(strSignal.value);
+///
+/// // or using the callable
+/// print(strSignal());
+///
+/// // or using `toString` implicit
+/// print("$strSignal");
+/// ```
+///
+/// You can set a new [value]:
+///
+/// ```dart
+/// // usign a `value` setter
+/// strSignal.value = "change value";
+///
+/// // or using the callable
+/// strSignal("change value");
+/// ```
+///
+/// You can use `Lifecycle.willUpdate` event for before the [value] is changed,
+/// or use `Lifecicle.didUpdate` event for after the [value] is changed:
+///
+/// ```dart
+/// Reactter.on(
+///   strSignal,
+///   Lifecycle.willUpdate,
+///   (signal, _) => print("Previous value: $signal"),
+/// );
+///
+/// Reactter.on(
+///   strSignal,
+///   Lifecycle.didUpdate,
+///   (signal, _) => print("Current value: $signal"),
+/// );
+/// ```
+///
+///
+/// If you use flutter, add [`flutter_reactter`](https://pub.dev/packages/flutter_reactter)
+/// package on your dependencies and use its Widgets.
+///
+/// See also:
+///
+/// * [Obj], a base-class that can be used to store a value of [T].
 class Signal<T> extends Obj<T> with ReactterNotifyManager, ReactterState {
   T _value;
 
@@ -30,7 +94,7 @@ class Signal<T> extends Obj<T> with ReactterNotifyManager, ReactterState {
   T call([T? val]) {
     assert(!_isDisposed, "You can call when it's been disposed");
 
-    if (val != null) value = val!;
+    if (val != null) value = val;
 
     return value;
   }
@@ -47,17 +111,22 @@ class Signal<T> extends Obj<T> with ReactterNotifyManager, ReactterState {
 }
 
 extension SignalNullExt<T> on Signal<T?> {
+  /// Returns a new `Signal<T>` with the value of the current `Signal<T?>`
+  /// if it's not null.
   Signal<T> get notNull => Signal<T>(value!);
 }
 
 extension SignalGenericTypeExt<T> on T {
+  /// Allows you to create a signal from any type.
   Signal<T> get signal => Signal<T>(this);
 }
 
 extension ObjToSignalExt<T> on Obj<T> {
+  /// Returns a new Signal<T> with the value of the current Obj<T>.
   Signal<T> get toSignal => Signal<T>(value);
 }
 
 extension SignalToObjExt<T> on Signal<T> {
+  /// Returns a new Obj<T> with the value of the current Signal<T>.
   Obj<T> get toObj => Obj<T>(value);
 }
