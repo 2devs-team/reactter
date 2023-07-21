e94c7cb23a501b8d49dd593fd0469d4d9cc6f80f
Carlos León <carlos.leon@meru.com>
2023-07-21 02:51:12 -0600

    build(widgets): Remove unnecesary import.

--- packages/flutter_reactter/lib/src/widgets.dart
+++ packages/flutter_reactter/lib/src/widgets.dart
@@ -7,7 +7,6 @@ import 'package:flutter_reactter/src/types.dart';
 import 'package:reactter/reactter.dart';
 
 import 'framework.dart';
-import 'internals.dart';
 
 part 'widgets/reactter_component.dart';
 part 'widgets/reactter_consumer.dart';
