fec39ac20b2f560f2cbe2ef7f550d5f86843bc07
Carlos León <carlos.leon@meru.com>
2024-04-10 03:20:38 -0600

    build: Add Tailwind CSS configuration and styles to the website.

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,11 +1,27 @@
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     starlight({
       title: "Reactter",
+      logo: {
+        src: "./src/assets/reactter.svg",
+      },
+      customCss: ["./src/styles/tailwind.css", "./src/styles/custom.css"],
+      defaultLocale: "root",
+      locales: {
+        root: {
+          label: "English",
+          lang: "en",
+        },
+        es: {
+          label: "Español",
+        },
+      },
       social: {
         github: "https://github.com/2devs-team/reactter",
       },
@@ -14,14 +30,22 @@ export default defineConfig({
           label: "Guides",
           items: [
             // Each item here is one entry in the navigation menu.
-            { label: "Example Guide", link: "/guides/example/" },
+            {
+              label: "Example Guide",
+              link: "/guides/example/",
+            },
           ],
         },
         {
           label: "Reference",
-          autogenerate: { directory: "reference" },
+          autogenerate: {
+            directory: "reference",
+          },
         },
       ],
+    }),
+    tailwind({
+      applyBaseStyles: false,
     }),
   ],
 });
