5e896c936d90f891cf1901967ea4f9c325cc288a
Carlos León <carlos.leon@meru.com>
2024-04-30 01:14:40 -0600

    build: Update site and base URLs in astro.config.mjs

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -7,6 +7,8 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://2devs-team.github.io",
+  base: "reactter",
   integrations: [
     starlight({
       title: "Reactter",
