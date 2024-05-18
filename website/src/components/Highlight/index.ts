460ae4199945d20c91c398135536fb9860a97718
Carlos León <carlos.leon@meru.com>
2024-05-18 02:42:26 -0600

    doc: Add `Highlight` components for code syntax highlighting

---
+++ website/src/components/Highlight/index.ts
@@ -0,0 +1,5 @@
+export { default as HE } from "./HE.astro";
+export { default as HK } from "./HK.astro";
+export { default as HM } from "./HM.astro";
+export { default as HN } from "./HN.astro";
+export { default as HT } from "./HT.astro";
