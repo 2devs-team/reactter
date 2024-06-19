38268d5749ab716cf678973208e6fb9bee05309d
Carlos León <carlos.leon@meru.com>
2024-06-19 17:38:59 -0600

    doc: Refactor to show example code as tabs and add ZAPP button component.

---
+++ website/src/examples/counter/lib/counter_view.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'counter.dart';
+
+class CounterView extends StatelessWidget {
+  const CounterView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Counter"),
+      ),
+      body: const Center(
+        child: Counter(),
+      ),
+    );
+  }
+}
