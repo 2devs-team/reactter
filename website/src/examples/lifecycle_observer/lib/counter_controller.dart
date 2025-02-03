426d0f84d75c3854b8a568e42c594acaff6625f6
Carlos León <carlos.leon@meru.com>
2025-02-03 01:22:11 -0600

    refactor(website): Improve `LifecycleObserver` documentation and examples for clarity and usability.

--- website/src/examples/lifecycle_observer/lib/counter_controller.dart
+++ website/src/examples/lifecycle_observer/lib/counter_controller.dart
@@ -3,39 +3,46 @@ import 'package:flutter_reactter/flutter_reactter.dart';
 class CounterController extends LifecycleObserver {
   final count = Signal(0);
 
-  void onInitialized() {
-    print('CounterController initialized');
-  }
-
-  void onDidMount() {
-    print('CounterController mounted');
-  }
-
-  void onWillMount() {
-    print('CounterController will mount');
-  }
-
-  void onWillUpdate(RtState state) {
-    print('CounterController will update by ${state.runtimeType}');
-  }
-
-  void onDidUpdate(RtState state) {
-    print('CounterController did update by ${state.runtimeType}');
-  }
-
-  void onWillUnmount() {
-    print('CounterController will unmount');
-  }
-
-  void onDidUnmount() {
-    print('CounterController did unmount');
-  }
-
   void increment() {
     count.value++;
   }
 
   void decrement() {
     count.value--;
+  }
+
+  @override
+  void onCreated() {
+    print('CounterController initialized');
+  }
+
+  @override
+  void onDidMount() {
+    print('CounterController mounted');
+  }
+
+  @override
+  void onWillMount() {
+    print('CounterController will mount');
+  }
+
+  @override
+  void onWillUpdate(RtState state) {
+    print('CounterController will update by ${state.runtimeType}');
+  }
+
+  @override
+  void onDidUpdate(RtState state) {
+    print('CounterController did update by ${state.runtimeType}');
+  }
+
+  @override
+  void onWillUnmount() {
+    print('CounterController will unmount');
+  }
+
+  @override
+  void onDidUnmount() {
+    print('CounterController did unmount');
   }
 }
